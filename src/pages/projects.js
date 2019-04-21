e80734d57ac5a830563665b1d5669cee6cf1cd53
Nick Lucas <me@nicklucas.co.uk>
2019-04-21 11:01:19 +0100

    Add prototype projects component

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,4 +1,33 @@
-import React from 'react'
-import Redirecting from 'components/Redirecting'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-export default () => <Redirecting to="/projects" />
+const ENDPOINT = '/.netlify/functions/github-projects'
+
+export default () => {
+  const [loadFailed, setLoadFailed] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get(ENDPOINT).then(response => {
+      if (response.status === 200) {
+        setData(response.data)
+        setLoading(false)
+      } else {
+        setLoadFailed(true)
+        setLoading(false)
+      }
+    })
+
+    // return () => {}
+  })
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+  if (loadFailed) {
+    return <div>Oh no, there was a problem loading this data</div>
+  }
+
+  return data.map(it => <div key={it.name}>{JSON.stringify(it)}</div>)
+}
